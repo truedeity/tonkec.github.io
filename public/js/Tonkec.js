23f8335d2401d32e7187e9c1f6a2f64457aec3ef
antonija <antonija1023@gmail.com>
2014-09-03 11:11:06 +0200

    add animate.css plugin

---
+++ public/js/Tonkec.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+	function animateMe(el, animation) {
+		$(window).scroll(function (){
+
+			var topOfWindow = $(window).scrollTop(),
+				bottomOfWindow = topOfWindow + $(window).height();
+
+				$(el).each(function(){
+
+					var elPosition = $(this).offset().top;
+
+					if (elPosition <= bottomOfWindow && elPosition >= topOfWindow) {
+						$(this).addClass(animation);
+					} else {
+						$(this).removeClass(animation);
+					}
+
+				}); 
+		});
+	};
+
+	animateMe('h1', 'animated bounce');
+	animateMe('.col-lg-4 span', 'animated shake');
+
+	$(window).scroll(function () {
+        var topOfWindow = $(window).scrollTop(),
+            bottomOfWindow = topOfWindow + $(window).height();
+
+        $('.progress .progress-bar').each(function () {
+            var elPosition = $(this).offset().top;
+
+            if(elPosition <= bottomOfWindow && elPosition >= topOfWindow){
+                $(this).progressbar({
+                	display_text: 'fill'
+                });
+            }
+        });
+    }); 
+
+
+})
