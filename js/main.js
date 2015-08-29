6ecdf34edf39d863a8c7e0e321d353ba98842563
antonija_simic <antonija1023@gmail.com>
2015-08-30 01:51:42 +0200

    add plugins

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
+$(window).load(function(){
+	  $('#preloader').delay(1000).fadeOut('slow'); 
+        $('body').delay(350).css({'overflow':'visible'});
 	$(".circle").click(function(){
 	  $(".circle-container").toggleClass("animated slideInDown visible");
 	});
+
+	var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    keyboardControl: true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    
+   })        
 });
