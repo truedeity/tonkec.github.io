0de28054c6a5405d17bf756e6d651c2ee5b6b396
antonija_simic <antonija1023@gmail.com>
2015-08-30 13:51:57 +0200

    add basic lyout for portfolio

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 $(window).load(function(){
-	  $('#preloader').delay(1000).fadeOut('slow'); 
-        $('body').delay(350).css({'overflow':'visible'});
-	$(".circle").click(function(){
-	  $(".circle-container").toggleClass("animated slideInDown visible");
-	});
+ $('#preloader').delay(1000).fadeOut('slow'); 
+ $('body').delay(350).css({'overflow':'visible'});
+ $(".circle").click(function(){
+     $(".circle-container").toggleClass("animated slideInDown visible");
+ });
 
-	var mySwiper = new Swiper ('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    keyboardControl: true,
-    pagination: '.swiper-pagination',
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    
-   })        
+ $(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+      }, 1000);
+        return false;
+    }
+}
+});
+});      
 });
