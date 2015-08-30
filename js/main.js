75831417d6812365af31b3681fb1324c68dcbed3
antonija_simic <antonija1023@gmail.com>
2015-08-30 16:12:26 +0200

    add working nav button

--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
 $(window).load(function(){
  $('#preloader').delay(1000).fadeOut('slow'); 
  $('body').delay(350).css({'overflow':'visible'});
- $(".circle").click(function(){
-     $(".circle-container").toggleClass("animated slideInDown visible");
+ var buttonCircles = $(".nav-circle");
+ 
+ $(".btn-floating").click(function(){
+    $(".circle-container").css("visibility", "visible")
+    $(this).toggleClass("closed-nav")
+    /*$(".first-circle").animate({
+        top: "+=50px"
+    })
+    $(".second-circle").animate({
+        top: "+=100px"
+    })
+    $(".third-circle").animate({
+        top: "+=150px"
+    })
+    $(".forth-circle").animate({
+        top: "+=200px"
+    })*/
+    if ($(this).hasClass("closed-nav")) {
+        slideDown();
+    } else {
+        slideUp()
+    }
+   
  });
+
+  function slideDown () {
+  var topMargin = 20
+  for (i = 0; i < buttonCircles.length; i++) { 
+      topMargin += 60
+      $(buttonCircles[i]).animate({
+        top: topMargin
+    })
+  }
+}
+
+function slideUp () {
+    for (i = 0; i < buttonCircles.length; i++) {
+      $(buttonCircles[i]).animate({
+        top: "20px"
+    }, 500, function(){
+         $(".circle-container").css("visibility", "hidden")
+    })
+  }
+}
 
  $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
@@ -16,7 +57,8 @@ $(window).load(function(){
       }, 1000);
         return false;
     }
-}
-});
-});      
+   }
+  });
+
+ });      
 });
