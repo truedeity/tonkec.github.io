3f4eb534b417bb7417cadb09dcbf09763a114806
antonija_simic <antonija1023@gmail.com>
2015-09-04 13:39:12 +0200

    fix animation on small screen and large screen, adjust vendors for firefox

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
 $(window).load(function(){
 
-  /* preloader */
- $('#preloader').delay(2000).fadeOut('slow', function(){
-  $("main").delay(500).css("visibility", "visible").fadeIn(2000);
-  $(".parallax-mirror").addClass("show-mirror");
- }); 
-
- $('body').delay(500).css({'overflow':'visible'});
-
+  runPreloader();
 /* animations only for large screen */
  if ($(window).width() > 1200) {
-    animate();
+    $(window).scrollTop(0);
     buttonOnLargeScreen();
+    type();
+    playVideo();
   } else {
-    animateArrow();
+    $(window).scrollTop(0);
+    animateArrow(3000);
     buttonOnSmallScreen();
   }
-
 
 /* swiper */
    var mySwiper = new Swiper ('.swiper-container', {
@@ -95,41 +90,57 @@ function slideUp() {
   });
  });
 
- /* modal */
-  $('.modal-trigger').leanModal();
-  $(".modal-close").click(function(event){
-    event.preventDefault();
-  });
-
-   function animateArrow () {
-    $(".btn-arrow").animate({
-          bottom: "+=150px"
-        }, 1, function(){
-          $(".fa-arrow-down").delay(1500).addClass("rotateIcon");
-          $("html,body").css("overflow", "visible");
-        });
+  function animateArrow (delay) {
+   $(".btn-arrow").animate({
+      bottom: "2%"
+    }, delay, function(){
+      $(".fa-arrow-down").delay(300).addClass("rotateIcon");
+      $("html,body").css("overflow", "visible");
+      animateButton();
+    });
    }
 
-/* starting animation */
-  function animate () {
-    function type() {
-     $(function(){
-       $(".element").typed({
-        strings: ['<h2 class="skill-title">Hello!</h2>My name is Antonija and I am <span class="skill-span">front-end developer</span>. My skillset mainly consists of <span class="skill-span">HTML</span>, <span class="skill-span">CSS3</span> and <span class="skill-span">jQuery</span>. Compiling <span class="skill-span">SASS</span> with  <span class="skill-span">Gulp</span> and versioning with <span class="skill-span">Git</span> is must-have. My biggest passions are <span class="skill-span">jQuery/CSS animations</span> and one pages. Front-end frameworks, such as <span class="skill-span">Bootstrap</span> and<span class="skill-span"> Materializecss</span> help me speed up my workflow. I code in <span class="skill-span">Linux Mint</span> and <span class="skill-span">SublimeText</span> equipped with <span class="skill-span">Emmet</span>. I am currently learning <span class="skill-span">Ruby on Rails</span> so <a href="mailto:antonija1023@gmail.com" class="skill-anchor"><span class="help-span"> I am looking  for RoR internship </span></a>.'],
-        typeSpeed: 0,
-        contentType: 'html',
-        startDelay: 8300,
-      });
-    }); 
-   } 
+function animateButton () {
+  $(".fixed-container").animate({
+    left: "0px"
+  }, 1500, function(){
 
-  
+  });
+}
 
-   var video = document.getElementById("video");
-   type();
-   video.play();
-   $("video").bind("ended", function() {
-    animateArrow();
-   });
-  }
+function runPreloader () {
+  $('#preloader').delay(2000).fadeOut('slow'); 
+  $('body').delay(500).css({'overflow':'visible'});
+  $("main").fadeIn(5000, function(){
+    $(".parallax-mirror").fadeIn(9000);
+  });
+}
+
+function type() {
+  $(function(){
+    $(".element").typed({
+      strings: ['<h2 class="skill-title">Hello!</h2>My name is Antonija and I am <span class="skill-span">front-end developer</span>. My skillset mainly consists of <span class="skill-span">HTML</span>, <span class="skill-span">CSS3</span> and <span class="skill-span">jQuery</span>. Compiling <span class="skill-span">SASS</span> with  <span class="skill-span">Gulp</span> and versioning with <span class="skill-span">Git</span> is must-have. My biggest passions are <span class="skill-span">jQuery/CSS animations</span> and one pages. Front-end frameworks, such as <span class="skill-span">Bootstrap</span> and<span class="skill-span"> Materializecss</span> speed up my workflow. I code in <span class="skill-span">Linux Mint</span> and <span class="skill-span">SublimeText</span> equipped with <span class="skill-span">Emmet</span>. I am currently learning <span class="skill-span">Ruby on Rails</span> so <a href="mailto:antonija1023@gmail.com" class="skill-anchor"><span class="help-span"> I am looking  for RoR internship </span></a>.'],
+      typeSpeed: 0,
+      contentType: 'html',
+      startDelay: 8300,
+      showCursor: true
+    });
+  }); 
+  } 
+function playVideo () {
+  var video = document.getElementById("video");
+  type();
+  video.play();
+  $("video").bind("ended", function() {
+    animateArrow(1);
+  });
+}
+
+
 });
+
+window.onresize = function(){ 
+  history.go(0);
+  window.location.href = window.location.href;
+  location.reload();
+}
